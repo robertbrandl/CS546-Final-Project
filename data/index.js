85344ed2d921b7141feb5fa7f9bf682ae831c93d
Robert Brandl <rbrandl@stevens.edu>
2023-11-13 20:43:26 -0500

    Began setting up the project structure

---
+++ data/index.js
@@ -0,0 +1,8 @@
+// This file should import both data files and export them as shown in the lecture code
+import userDataFunctions from './users.js';
+import reviewDataFunctions from './reviews.js';
+import showDataFunctions from './shows.js';
+
+export const userData = userDataFunctions;
+export const reviewData = reviewDataFunctions;
+export const showData = showDataFunctions;
