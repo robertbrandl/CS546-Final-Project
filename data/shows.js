97d4d22a3a102cefc6963eba2b5048e69ee5dad3
Robert Brandl <rbrandl@stevens.edu>
2023-11-18 20:17:00 -0500

    Began data functions

---
+++ data/shows.js
@@ -0,0 +1,28 @@
+import * as validation from "../validation.js";
+import {ObjectId} from 'mongodb';
+import {shows} from '../config/mongoCollections.js';
+
+const getAllShows = async () => {
+
+}
+const searchForShow = async() => {
+
+}
+const sortByGenre = async () => {
+
+}
+const sortByRuntime = async () => {
+
+}
+const sortByRating = async () => {
+
+}
+const sortByRewatchPercent = async () => {
+
+}
+const getIndividualShow = async () =>{
+
+}
+const getSimilarShows = async () =>{
+
+}
