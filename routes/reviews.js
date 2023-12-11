b2a4338ce71ec73a4e49f978acde3256befc6c3b
slinford22 <148028571+slinford22@users.noreply.github.com>
2023-12-11 17:44:03 -0500

    more review routes changes

--- routes/reviews.js
+++ routes/reviews.js
@@ -79,8 +79,8 @@ router
 });
 
 router
-.route('/:id')
-//.get to get the review itself?
+.route('/account/:id')
+//get to get all reviews? need to add data function
 .delete(async (req,res) => {
     //remove a review
     //req.params.id is review id
@@ -96,51 +96,31 @@ router
         if (!ObjectId.isValid(rId)) throw 'invalid review ID';
     }
     catch(e) {
-        //this will redirect back to the review's page
-        return res
-            .status(400)
-            .json(e);
+        res.redirect('error', {code:'400',errorText:'invalid review ID'});
     }
     try {
         let deletedReview = await reviewData.remove(reviewId);
-        //this will render "delete successful" on the review's page
-        return res
-            .status(200)
-            .json(deletedReview);
+        if (deletedReview !== undefined) {
+            return res.redirect('/account');
+        }
     }
     catch(e) {
-        //this will render "delete unsuccessful" on the review's page
-        return res
-            .status(404)
-            .json({error: e});
+        res.redirect('error', {code:'404',errorText:'review could not be deleted'});
     }
 })
 .put(async (req,res) => {
     //update a review
     const reviewId = req.params.id.trim();
     const reviewInput = req.body;
-    if (!reviewInput || Object.keys(reviewInput).length === 0) {
-        return res
-          .status(400)
-          .json({error: 'There are no fields in the request body'});
-    }
     const userInfo = req.session.user;
     if (!userInfo) {
         //user is not logged in
-        return res
-            .status(401)
-            .json({error: 'User is not logged in'});
+        res.redirect('/login');
     }
     //data validation
     try {
         let rId = validation.checkString(reviewId);
         if (!ObjectId.isValid(rId)) throw 'invalid review id';
-        let sId = validation.checkString(reviewInput.show_id);
-        if (!ObjectId.isValid(sId)) throw 'invalid show ID';
-        let uId = validation.checkString(req.session.user._id);
-        if (!ObjectId.isValid(uId)) throw 'invalid user ID';
-        let fname = validation.checkString(req.session.user.firstName);
-        let lname = validation.checkString(req.session.user.lastName);
         let til = validation.checkString(reviewInput.title);
         if (reviewInput.rating === undefined || reviewInput.rating === null || !reviewInput.rating){
             throw "The rating is not supplied, null, or undefined";
@@ -153,31 +133,23 @@ router
         if (typeof reviewInput.watchAgain !== "boolean"){throw "watchAgain is not a boolean"}
     }
     catch(e) {
-        return res
-            .status(400)
-            .json({error: e});
+        res.render('error',{code:'400',errorText:e});
     }
     try {
         //try to update
         const updated = await reviewData.update(
                 reviewId,
-                reviewInput.show_id,
-                req.session.user._id,
-                req.session.user.firstName,
-                req.session.user.lastName,
                 reviewInput.title,
                 reviewInput.rating,
                 reviewInput.content,
                 reviewInput.watchAgain
             );
-            return res
-                .status(200)
-                .json(updated);
+        if (updated !== undefined) {
+            return res.redirect('/account');
+        }  
     }
     catch(e) {
-        return res
-            .status(404)
-            .json({error:e});
+        res.redirect('error', {code:'404',errorText:'review could not be deleted'});
     }
 });
 
